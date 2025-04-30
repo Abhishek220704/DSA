0fe7d865d0b536687fa3e6d551db127552abcda1
ABHISHEK WEKHANDE <144373509+Abhishek220704@users.noreply.github.com>
2025-04-30 22:22:01 +0530

    if else loop.cpp

---
+++ Basics/if else loop.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+/*
+1) // write a code for is a guy eligible to vote based on the age
+int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<18){
+        cout << "U can't vote";
+    }
+    else{
+        cout << "U can vote";
+    }
+return 0;
+}
+
+
+2) // create a grading system based on the marks he scored out of 100
+int main() {
+    int marks;
+    cout << "ENTER YOUR MARKS: ";
+    cin >> marks;
+
+    if (marks > 100 || marks < 0) {
+        cout << "Invalid marks";
+    }
+    else if (marks >= 90) {
+        cout << "A";
+    }
+    else if (marks >= 80) {
+        cout << "B";
+    }
+    else if (marks >= 70) {
+        cout << "C";
+    }
+    else if (marks >= 60) {
+        cout << "D";
+    }
+    else if (marks >= 50) {
+        cout << "E";
+    }
+    else {
+        cout << "Fail";
+    }
+
+    return 0;
+}
+
+3) // write a code that is a person eligible for job
+int main(){
+    int age;
+    cout << "ENTRE YOUR AGE : ";
+    cin >> age;
+    if(age<18){
+        cout << "not eligible";
+    }
+    else if(age>=18 && age<=54){
+        cout << "Eligible";
+    }
+    else if(age>54 && age<60){
+        cout << "Retirement soon";
+    }
+    else{
+        cout << "retired";
+    }
+    return 0;
+    }
+*/
