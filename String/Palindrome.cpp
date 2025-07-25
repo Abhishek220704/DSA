9a29e10aee95da1e24cd09938aa122aaefb082a9
ABHISHEK WEKHANDE <144373509+Abhishek220704@users.noreply.github.com>
2025-07-26 00:04:18 +0530

    Create Palindrome.cpp

---
+++ String/Palindrome.cpp
@@ -0,0 +1,15 @@
+string palidrome(string s){
+  int left = 0;
+  int right = s.size()-1;
+  while(left<right){
+    if(left != right) return false;
+      left ++;
+      right --;
+    }
+    return true;
+}
+int main(){
+  cin >> s;
+  palidrome(s);
+  return false;
+}
