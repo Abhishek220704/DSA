a569740184a8884134841d379a17ddcb151bb682
ABHISHEK WEKHANDE <144373509+Abhishek220704@users.noreply.github.com>
2025-07-25 23:43:33 +0530

    Create Remove outermost Paranthesis.cpp

---
+++ String/Remove outermost Paranthesis.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+string outermost(string s){
+  int balance = 0;
+  int n = s.size();
+  for(int i=0;i<n;i++){
+    if(s[i] == '(') balance++;
+    if(s[i] == ')') balance--;
+
+    if(balance == 0 && i != s.size() - 1) return s;
+  }
+  return s.substr(1,s.size() - 2);
+}
+
+int main(){
+  string s = "(()())(())(()())";
+  cout << outermost(s);
+  return 0;
+}
