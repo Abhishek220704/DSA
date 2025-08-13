f524e05235bad9f3d72e917de7ef4ac3510257ca
ABHISHEK WEKHANDE <144373509+Abhishek220704@users.noreply.github.com>
2025-08-13 19:57:12 +0530

    Create check whether one string is a rotation of another.cpp

---
+++ String/check whether one string is a rotation of another.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool rotatestring(const string &s, const string &goal){
+  if(s.length() != goal.length()) return false;
+  string doubled = s + s;
+  return doubled.find(goal) != string::npos;
+}
+int main(){
+  string s,goal;
+  cin >> s;
+  cin >> goal;
+  if(rotatestring(s,goal)){
+    cout << "True";
+  }
+  else{
+    cout << "False";
+  }
+return 0;
+}
