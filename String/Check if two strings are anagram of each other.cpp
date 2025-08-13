e4fa07aa2345a87d19952919eb8df22ed370eb7f
ABHISHEK WEKHANDE <144373509+Abhishek220704@users.noreply.github.com>
2025-08-13 20:11:07 +0530

    Create Check if two strings are anagram of each other.cpp

---
+++ String/Check if two strings are anagram of each other.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+bool anagramcheck(count string &s, count string &t){
+  if(s.length() != t.length()) return false;
+  vector<int> count(26,0);
+  for(size_t i=0; i<= s.length(); i++){
+    count[s[i] - 'a']++;
+    count[t[i] - 'a']--;
+  }
+  for(int freq : count){
+    if(freq != 0) return false;
+  }
+  return true;
+}
+int main(){
+  string s,t;
+  cin >> s >> t;
+  if(anagramcheck(s,t)){
+    cout << "True";
+  }
+  else{
+    cout << "False";
+  }
+return 0;
+}
+  
